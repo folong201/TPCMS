158478ddd54f95e1d289e130ef1c364af3330eec
folongemerson <folongemersonwell@gmail.com>
2022-06-01 11:01:41 +0100

    connection with the frontend part

---
+++ frontCms/index.php
@@ -0,0 +1,419 @@
+<?php
+    echo"kniw is the php time";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mairie Yde IV</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo">
+        <img src="images/b2.jpeg" alt="">
+    </a>
+
+    <nav class="navbar">
+        <a href="#home">Accueil</a>
+        <a href="projets.html">Projets</a>
+        <a href="activites.html">Activites</a>
+        <a href="annonces.html">Annonces</a>
+        <a href="lieux.html">Lieux touristiques</a>
+        <a href="pub.html">Pub</a>
+    </nav>
+<!-- 
+    <div class="icons">
+        <div class="fas fa-search" id="search-btn"></div>
+        <div class="fas fa-shopping-cart" id="cart-btn"></div>
+        <div class="fas fa-bars" id="menu-btn"></div>
+    </div>
+
+    <div class="search-form">
+        <input type="search" id="search-box" placeholder="search here...">
+        <label for="search-box" class="fas fa-search"></label>
+    </div> -->
+    <div class="relate">
+        .
+    </div>
+
+    <div class="cart-items-container">
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/cart-item-1.png" alt="">
+            <div class="content">
+                <h3>cart item 01</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/cart-item-2.png" alt="">
+            <div class="content">
+                <h3>cart item 02</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/cart-item-3.png" alt="">
+            <div class="content">
+                <h3>cart item 03</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/cart-item-4.png" alt="">
+            <div class="content">
+                <h3>cart item 04</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <a href="#" class="btn">checkout now</a>
+    </div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>Bienvenue a la Mairie de Yaounde IV</h3>
+        <p>La Commune d'Arrondissement de Yaounde IV est située dans la Région du Centre, précisément au sud de la ville de Yaoundé et est implantée sur une superficie estimée environ à 5000 m2. Elle couvre en termes d’unité de commandement une superficie estimée à 57 km2.</p>
+        <a href="#" class="btn">Lire Plus</a>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>Presentation de </span> l'arrondissement de Yaounde IV </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="images/b4.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>Histoire de la commune de la ville, Yaoundé4</h3>
+            <p>Retrouver tous les services et activités disponibles dans la commune de yaounde IV pour votre bien être et votre épanouissement.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nemo illo sequi et nostrum perspiciatis fuga rem mollitia molestiae? Fugiat velit quo dignissimos deserunt sequi quasi labore quis vero placeat.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab obcaecati corporis rem iure distinctio sequi temporibus, aliquid facilis numquam ipsa vitae, in incidunt odio molestiae qui deserunt commodi dolores debitis.
+            </p>
+            <a href="#" class="btn">voir plus</a>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- menu section starts  -->
+
+<section class="review" id="review">
+
+   
+    <h1 class="heading"> Personnel <span>de la </span> Mairie </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/quote-img.png" alt="" class="quote">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nulla sit libero nemo fuga sequi nobis? Necessitatibus aut laborum, nisi quas eaque laudantium consequuntur iste ex aliquam minus vel? Nemo.</p>
+            <img src="images/b2.jpeg" class="user" alt="">
+            <h3>john deo | <span> Maire </span> </h3>
+            <h3>32 Ans </h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/quote-img.png" alt="" class="quote">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nulla sit libero nemo fuga sequi nobis? Necessitatibus aut laborum, nisi quas eaque laudantium consequuntur iste ex aliquam minus vel? Nemo.</p>
+            <img src="images/b3.jpeg" class="user" alt="">
+            <h3>john deo | <span>Secretaire</span></h3>
+            <h3>32 Ans </h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+        </div>
+        
+        <div class="box">
+            <img src="images/quote-img.png" alt="" class="quote">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nulla sit libero nemo fuga sequi nobis? Necessitatibus aut laborum, nisi quas eaque laudantium consequuntur iste ex aliquam minus vel? Nemo.</p>
+            <img src="images/b3.jpeg" class="user" alt="">
+            <h3>john deo | <span>1er Adjoin</span></h3>
+            <h3>32 Ans </h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+
+<!-- menu section ends -->
+
+<section class="products" id="products">
+
+ <h1 class="heading"> Conseil Municipale <span>de La Mairie</span> </h1>
+    <div class="box-container">
+            <?php
+                $database = new PDO('mysql:host=localhost;dbname=CMS','root','');
+                $sql = "SELECT * FROM membre";
+                $reponce = $database->query($sql);
+                while($data = $reponce->fetch()){
+                    
+                    ?>
+                    
+                    
+                    <div class="box">
+            <div class="icons">
+                <a href="#" class="fas fa-shopping-cart"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+            <div class="image">
+                <img src="../admin/upload/<?php echo$data['photo']; ?>" alt="">
+            </div>
+            <div class="content">
+                <h3><?php echo$data['name']; ?> | Maire</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price">50 Ans </div>
+            </div>
+        </div>
+                    
+                    
+                    <?php
+                    
+                }
+            ?>
+        <div class="box">
+            <div class="icons">
+                <a href="#" class="fas fa-shopping-cart"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+            <div class="image">
+                <img src="images/b3.jpeg" alt="">
+            </div>
+            <div class="content">
+                <h3>fresh coffee | Maire</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price">50 Ans </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="icons">
+                <a href="#" class="fas fa-shopping-cart"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+            <div class="image">
+                <img src="images/b3.jpeg" alt="">
+            </div>
+            <div class="content">
+                <h3>fresh coffee | Maire</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price">50 Ans </div>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="icons">
+                <a href="#" class="fas fa-shopping-cart"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+            <div class="image">
+                <img src="images/b3.jpeg" alt="">
+            </div>
+            <div class="content">
+                <h3>fresh coffee | Maire</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <div class="price">50 Ans </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- review section starts  -->
+
+
+<!-- review section ends -->
+
+<!-- contact section starts  -->
+
+<!-- contact section ends -->
+
+<!-- blogs section starts  -->
+<section class="blogs" id="blogs">
+
+    <h1 class="heading"> Notre <span>Mission</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="images/blog-1.jpeg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">tasty and refreshing coffee</a>
+                <span>by admin / 21st may, 2021</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dicta.</p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/blog-2.jpeg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">tasty and refreshing coffee</a>
+                <span>by admin / 21st may, 2021</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dicta.</p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/blog-3.jpeg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">tasty and refreshing coffee</a>
+                <span>by admin / 21st may, 2021</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dicta.</p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blogs section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="share">
+        <a href="#" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        <a href="#" class="fab fa-pinterest"></a>
+    </div>
+
+    <div class="links">
+        <a href="#active" id="active">home</a>
+        <a href="#">about</a>
+        <a href="#">menu</a>
+        <a href="#">products</a>
+        <a href="#">review</a>
+        <a href="#">contact</a>
+        <a href="#">blogs</a>
+    </div>
+
+  
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    header('Location: frontCms/index.php');
+    exit;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
